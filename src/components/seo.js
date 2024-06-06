8639adc03e1acc5acd72aa65c79378ed47a96db7
InnaShp <107990414+InnaShp@users.noreply.github.com>
2024-06-06 17:21:33 +0300

    Add identity-widget

--- src/components/seo.js
+++ src/components/seo.js
@@ -43,6 +43,7 @@ const Seo = ({ description, title, children }) => {
       />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={metaDescription} />
+      <script type="text/javascript" src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       {children}
     </>
   )
